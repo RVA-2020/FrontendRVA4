6314fcbd494c35b24e3178ff3197a289dc98e74a
username** <marko.bojkic@uns.ac.rs>
2020-05-12 17:00:18 +0200

    Vezbe 9

--- FrontendRVA4/src/app/components/artikl/artikl.component.ts
+++ FrontendRVA4/src/app/components/artikl/artikl.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ArtiklService } from './../../services/artikl.service';
+import { Artikl } from './../../models/artikl';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-artikl',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtiklComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['id', 'naziv', 'proizvodjac', 'actions'];
+  dataSource: MatTableDataSource<Artikl>;
+
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+
+  constructor(private artiklService: ArtiklService) { }
+
+  public loadData() {
+    this.artiklService.getAllArtikl().subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // otklanjanje razmaka
+    filterValue = filterValue.toLocaleLowerCase(); // mala slova
+    this.dataSource.filter = filterValue; //    MoJa   --> MoJa --> moja
   }
 
 }
---
+++ FrontendRVA4/src/app/services/artikl.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Artikl } from '../models/artikl';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArtiklService {
+
+  private readonly API_URL = 'http://localhost:8083/artikl/';
+
+  dataChange: BehaviorSubject<Artikl[]> = new BehaviorSubject<Artikl[]>([]);
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllArtikl(): Observable<Artikl[]> {
+    this.httpClient.get<Artikl[]>(this.API_URL).subscribe(data => {
+      this.dataChange.next(data);
+    },
+    (error: HttpErrorResponse) => {
+      console.log(error.name + ' --> ' + error.message);
+    });
+
+    return this.dataChange.asObservable();
+  }
+
+  public addArtikl(artikl: Artikl): void {
+    this.httpClient.post(this.API_URL, artikl);
+  }
+
+  public updateArtikl(artikl: Artikl): void {
+    this.httpClient.put(this.API_URL, artikl);
+  }
+
+  public deleteArtikl(id: number): void {
+    this.httpClient.delete(this.API_URL + id);
+    console.log('Deleting Artikl with ID: ' + id);
+  }
+
+}
